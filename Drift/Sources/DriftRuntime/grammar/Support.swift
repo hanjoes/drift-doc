a88812c53f1f61bef23a638f9d27ce106ee253dd
Hanzhou Shi <hanzhou87@gmail.com>
2017-07-07 21:53:46 -0700

    Implemented support method to check inlineTag.

--- Drift/Sources/DriftRuntime/grammar/Support.swift
+++ Drift/Sources/DriftRuntime/grammar/Support.swift
@@ -2,6 +2,18 @@ import Antlr4
 
 class Support {
     public static func isNotInlineTagStart(_ input: TokenStream) -> Bool {
-        return true
+        let maybeNextToken = try! input.LT(1)
+        
+        guard let nextToken = maybeNextToken else {
+            return true
+        }
+        guard (nextToken.getText())! == "{" else {
+            return true
+        }
+        guard let afterNextToken = try! input.LT(2) else {
+            return true
+        }
+        
+        return (afterNextToken.getText())! != "@"
     }
 }
