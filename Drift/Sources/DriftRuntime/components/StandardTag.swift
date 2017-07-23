207ec074764e923f6fdc80afae456087f51685ce
Hanzhou Shi <hanzhou87@gmail.com>
2017-07-23 13:44:04 -0700

    Cleaned up comments.

--- Drift/Sources/DriftRuntime/components/StandardTag.swift
+++ Drift/Sources/DriftRuntime/components/StandardTag.swift
@@ -27,6 +27,24 @@ extension StandardTag {
         }
     }
     
+    /// Available Javadoc tags.
+    /// Although we don't use all of them.
+    ///
+    /// +--------------+-------------+
+    /// |@author       | 1.0         |
+    /// |@deprecated   | 1.0         |
+    /// |@exception    | 1.0         |
+    /// |@param        | 1.0         |
+    /// |@return       | 1.0         |
+    /// |@see          | 1.0         |
+    /// |@serial       | 1.2         |
+    /// |@serialData   | 1.2         |
+    /// |@serialField  | 1.2         |
+    /// |@since        | 1.1         |
+    /// |@throws       | 1.2         |
+    /// |@version      | 1.0         |
+    /// +--------------+-------------+
+    ///
     var markup: SwiftMarkupOutputModel {
         switch name {
         case "param":
@@ -46,67 +64,9 @@ extension StandardTag {
         default:
             return SwiftMarkupDescription.text(children.map{$0.markup.description}.joined(separator: ""))
         }
-    //        var callout: Callout
-    //        switch name {
     //        case "deprecated":
     //            break
     //        case "see":
-    //            break
-    //        return "\(callout)"
     }
 }
-
-
-// MARK: SwiftMarkupConvertible
-//extension StandardTag {
-    /// Available Javadoc tags.
-    ///
-    /// +--------------+-------------+
-    /// |@author       | 1.0         |
-    /// |@deprecated   | 1.0         |
-    /// |@exception    | 1.0         |
-    /// |@param        | 1.0         |
-    /// |@return       | 1.0         |
-    /// |@see          | 1.0         |
-    /// |@serial       | 1.2         |
-    /// |@serialData   | 1.2         |
-    /// |@serialField  | 1.2         |
-    /// |@since        | 1.1         |
-    /// |@throws       | 1.2         |
-    /// |@version      | 1.0         |
-    /// +--------------+-------------+
-    ///
-//    var markup: String {
-//        var callout: Callout
-//        switch name {
-//        case "author":
-//            callout = Author(content: childrenMarkup)
-//        case "deprecated":
-//            break
-//        case "exception":
-//            break
-//        case "param":
-//            callout = Parameter(content: childrenMarkup)
-//        case "return":
-//            callout = Returns(content: childrenMarkup)
-//        case "see":
-//            break
-//        case "serial":
-//            break
-//        case "serialData":
-//            break
-//        case "serialField":
-//            break
-//        case "since":
-//            callout = Since(content: childrenMarkup)
-//        case "throws":
-//            callout = Throws(content: childrenMarkup)
-//        case "version":
-//            callout = Version(content: childrenMarkup)
-//        default:
-//            callout = Noop(content: childrenMarkup)
-//        }
-//        return "\(callout)"
-//    }
-//}
 
